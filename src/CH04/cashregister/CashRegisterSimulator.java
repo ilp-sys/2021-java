7ec55fd2a6db818bf7f5e8295985c9e84ba20558
z2ouu <ikwydls1314@gmail.com>
2021-03-31 13:50:48 +0900

    week 5

--- src/CH04/cashregister/CashRegisterSimulator.java
+++ src/CH04/cashregister/CashRegisterSimulator.java
@@ -1,5 +1,6 @@
 package CH04.cashregister;
 
+import javax.swing.*;
 import java.util.Scanner;
 
 public class CashRegisterSimulator {
@@ -7,11 +8,30 @@ public class CashRegisterSimulator {
         Scanner in = new Scanner(System.in);
         CashRegister register = new CashRegister();
 
-        System.out.println("Enter Price");
-        double price = in.nextDouble();
+        //System.out.println("Enter Price");
+        String input = JOptionPane.showInputDialog("Enter Price: ");
+        double price = Double.parseDouble(input); //in.nextDouble();
         register.recordPurchase(price);
 
-        System.out.println("Enter dollars: ");
-        int dollars = in.nextInt();
+        //System.out.println("Enter dollars: ");
+        input = JOptionPane.showInputDialog("Enter Dollars: ");
+        int dollars = Integer.parseInt(input); //in.nextInt();
+
+        input = JOptionPane.showInputDialog("Enter quarters: ");
+        int quarters = Integer.parseInt(input);
+
+        input = JOptionPane.showInputDialog("Enter dimes: ");
+        int dimes = Integer.parseInt(input);
+
+        input = JOptionPane.showInputDialog("Enter nickels: ");
+        int nickels = Integer.parseInt(input);
+
+        input = JOptionPane.showInputDialog("Enter pennies: ");
+        int pennies = Integer.parseInt(input);
+        register.receivePayment(dollars, quarters, dimes, nickels, pennies);
+
+        JFrame f = new JFrame();
+        JOptionPane.showMessageDialog(f, "Your change: " + register.giveChange());
+        System.exit(0);
     }
 }
--- src/CH04/cashregister/CashRegisterTester.java
+++ src/CH04/cashregister/CashRegisterTester.java
@@ -6,15 +6,13 @@ public class CashRegisterTester{
         register.recordPurchase(0.75);
         register.recordPurchase(1.5);
         register.receivePayment(2,0,5,0,0);
-        System.out.println("Change: ");
-        System.out.println(register.giveChange());
+        System.out.println("Change: " + register.giveChange());
         System.out.println("Expected: 0.25");
 
         register.recordPurchase(2.25);
         register.recordPurchase(19.25);
         register.receivePayment(23, 2, 0,0,0);
-        System.out.println("Change: ");
-        System.out.println(register.giveChange());
+        System.out.println("Change: " + register.giveChange());
         System.out.println("Expected: 2.0");
 
         System.out.println("Value of Quater is " + CashRegister.QUARTER_VALUE);
